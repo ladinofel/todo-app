46c583e78440846a6b8886e99152914857c4b406
Andrés Ladino <122582963+ladinofel@users.noreply.github.com>
2024-03-31 19:34:26 -0500

    Add header-options and box shadow for left-column

---
+++ src/components/headerPanel.js
@@ -0,0 +1,34 @@
+import Add from '/src/icons/add-circle-outline.svg';
+
+const headerGenerator = (() => {
+
+  const header = document.querySelector('#header');
+
+  const headerOptions = document.createElement('div');
+  headerOptions.classList.add('header-options');
+
+  const newProject = document.createElement('div');
+  newProject.classList.add('header-option');
+  const projectIcon = new Image();
+  projectIcon.src = Add;
+  const newProjectTitle = document.createElement('a');
+  newProjectTitle.classList.add('icon-text');
+  newProjectTitle.textContent = 'New Project';
+
+  const newTask = document.createElement('div');
+  newTask.classList.add('header-option');
+  const taskIcon = new Image();
+  taskIcon.src = Add;
+  const newTaskTitle = document.createElement('a');
+  newTaskTitle.classList.add('icon-text');
+  newTaskTitle.textContent = 'New Task';
+
+  newProject.append(projectIcon, newProjectTitle);
+  newTask.append(taskIcon, newTaskTitle);
+  headerOptions.append(newProject, newTask);
+  header.append(headerOptions);
+
+
+})();
+
+export default headerGenerator;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './styles.css';
 import menuGenerator from './components/leftPanel';
 import projectGenerator  from './components/leftPanel';
+import headerGenerator from './components/headerPanel';

d27466a3fc4c7a8c56ef893e44a717ce1e049303
Andrés Ladino <122582963+ladinofel@users.noreply.github.com>
2024-06-09 16:44:30 -0500

    Add project toggle btns to leftMenu

--- src/components/leftPanel.js
+++ src/components/leftPanel.js
@@ -19,6 +19,7 @@ const menuGenerator = (() => {
   const allInput = document.createElement('input');
   allInput.setAttribute('type', 'checkbox');
   allInput.id = 'switch-all';
+  allInput.checked = true;
   const allSpan = document.createElement('span');
   allSpan.classList.add('slider-round');
   const displayAllTitle = document.createElement('p');
@@ -99,16 +100,98 @@ const projectGenerator = (() => {
   const projectMenu = document.createElement('div');
   projectMenu.classList.add('project-menu');
 
-  const projectHeader = document.createElement('div');
-  projectHeader.classList.add('project-header');
-  const projectIcon = new Image();
-  projectIcon.src = Project;
-  const projectTitle = document.createElement('p');
-  projectTitle.classList.add('project-title');
-  projectTitle.textContent = 'PROJECTS';
+  const allProjectsDisplay = document.createElement('div');
+  allProjectsDisplay.classList.add('display-option');
+  const allProjectsLabel = document.createElement('label');
+  allProjectsLabel.classList.add('switch');
+  const allProjectsInput = document.createElement('input');
+  allProjectsInput.setAttribute('type', 'checkbox');
+  allProjectsInput.id = 'switch-work';
+  allProjectsInput.checked = true;
+  const allProjectsSpan = document.createElement('span');
+  allProjectsSpan.classList.add('slider-round');
+  const allProjectsDisplayTitle = document.createElement('p');
+  allProjectsDisplayTitle.textContent = 'All';
 
-  projectHeader.append(projectIcon, projectTitle);
-  projectMenu.append(projectHeader);
+  allProjectsLabel.append(allProjectsInput, allProjectsSpan);
+  allProjectsDisplay.append(allProjectsLabel, allProjectsDisplayTitle);
+  
+  const workDisplay = document.createElement('div');
+  workDisplay.classList.add('display-option');
+  const workLabel = document.createElement('label');
+  workLabel.classList.add('switch');
+  const workInput = document.createElement('input');
+  workInput.setAttribute('type', 'checkbox');
+  workInput.id = 'switch-work';
+  const workSpan = document.createElement('span');
+  workSpan.classList.add('slider-round');
+  const workDisplayTitle = document.createElement('p');
+  workDisplayTitle.textContent = 'Work';
+
+  workLabel.append(workInput, workSpan);
+  workDisplay.append(workLabel, workDisplayTitle);
+
+  const homeDisplay = document.createElement('div');
+  homeDisplay.classList.add('display-option');
+  const homeLabel = document.createElement('label');
+  homeLabel.classList.add('switch');
+  const homeInput = document.createElement('input');
+  homeInput.setAttribute('type', 'checkbox');
+  homeInput.id = 'switch-home';
+  const homeSpan = document.createElement('span');
+  homeSpan.classList.add('slider-round');
+  const homeDisplayTitle = document.createElement('p');
+  homeDisplayTitle.textContent = 'Home';
+
+  homeLabel.append(homeInput, homeSpan);
+  homeDisplay.append(homeLabel, homeDisplayTitle);
+
+  const schoolDisplay = document.createElement('div');
+  schoolDisplay.classList.add('display-option');
+  const schoolLabel = document.createElement('label');
+  schoolLabel.classList.add('switch');
+  const schoolInput = document.createElement('input');
+  schoolInput.setAttribute('type', 'checkbox');
+  schoolInput.id = 'switch-school';
+  const schoolSpan = document.createElement('span');
+  schoolSpan.classList.add('slider-round');
+  const schoolDisplayTitle = document.createElement('p');
+  schoolDisplayTitle.textContent = 'School';
+
+  schoolLabel.append(schoolInput, schoolSpan);
+  schoolDisplay.append(schoolLabel, schoolDisplayTitle);
+
+  const personalDisplay = document.createElement('div');
+  personalDisplay.classList.add('display-option');
+  const personalLabel = document.createElement('label');
+  personalLabel.classList.add('switch');
+  const personalInput = document.createElement('input');
+  personalInput.setAttribute('type', 'checkbox');
+  personalInput.id = 'switch-personal';
+  const personalSpan = document.createElement('span');
+  personalSpan.classList.add('slider-round');
+  const personalDisplayTitle = document.createElement('p');
+  personalDisplayTitle.textContent = 'Personal';
+
+  personalLabel.append(personalInput, personalSpan);
+  personalDisplay.append(personalLabel, personalDisplayTitle);
+
+  const otherDisplay = document.createElement('div');
+  otherDisplay.classList.add('display-option');
+  const otherLabel = document.createElement('label');
+  otherLabel.classList.add('switch');
+  const otherInput = document.createElement('input');
+  otherInput.setAttribute('type', 'checkbox');
+  otherInput.id = 'switch-other';
+  const otherSpan = document.createElement('span');
+  otherSpan.classList.add('slider-round');
+  const otherDisplayTitle = document.createElement('p');
+  otherDisplayTitle.textContent = 'Other';
+
+  otherLabel.append(otherInput, otherSpan);
+  otherDisplay.append(otherLabel, otherDisplayTitle);
+
+  projectMenu.append(allProjectsDisplay, workDisplay, homeDisplay, schoolDisplay, personalDisplay, otherDisplay);
   leftColumn.append(projectMenu);
 
 })();
